8d3a5f8da73ed301483cbde830170e875043293a
samuel5719 <sammu080603@gmail.com>
2024-07-02 18:40:40 +0000

    Mundo guardado 2024-07-02

--- server.py
+++ server.py
@@ -4,17 +4,7 @@
 # Puedes instalar mohist después de instalar forge desde el menú de gestionar
 # Puedes instalar paper después de instalar vanilla desde el menú de gestionar
 # Puedes instalar purpur después de instalar fabric desde el menú de gestionar
-
-# Regiones de ngrok
-# Código          Lugar
-#-----------      ---------------------------
-# ap	          Asia / Pacífico (Singapore)
-# au		      Australia (Sydney)
-# eu		      Europa (Frankfurt)
-# in		      India (Mumbai)
-# jp		      Japón (Tokyo)
-# sa		      Sudamérica (São Paulo)
-# us		      Estados unidos (Ohio)
+3
 # us-cal-1	      Estados unidos (California)
 
 
